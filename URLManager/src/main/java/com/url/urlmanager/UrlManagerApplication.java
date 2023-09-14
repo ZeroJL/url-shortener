bba78a693da83f196f4f199d7340ed39dac25b92
ZeroJL <youngjae991@gmail.com>
2023-09-14 21:40:47 +0900

    refactor: change multi module for #5

---
+++ URLManager/src/main/java/com/url/urlmanager/UrlManagerApplication.java
@@ -0,0 +1,13 @@
+package com.url.urlmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlManagerApplication.class, args);
+    }
+
+}
---
+++ URLManager/src/test/java/com/url/urlmanager/UrlManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.url.urlmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
